c9586aaf2ffbb522ed309a3dd38fd42c46116ff6
shokh <1mustafoman@gmail.com>
2022-03-21 18:08:29 +0500

    Initial Commit

---
+++ instaInter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  instaInter
+//
+//  Created by Shohjahon Numonovich on 3/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
